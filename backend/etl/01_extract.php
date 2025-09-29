16bcfbd5e1b1be0d200fc87e050a88b0bacee30e
kmoserfhgr <kaya.moser@stud.fhgr.ch>
2025-09-29 14:54:08 +0200

    APL

---
+++ backend/etl/01_extract.php
@@ -0,0 +1,26 @@
+<?php
+
+function fetchEnergyRadioData() {
+    $url = "https://energy.ch/api/channels/bern/playouts";
+
+    // Initialisiert eine cURL-Sitzung
+    $ch = curl_init($url);
+
+    // Setzt Optionen
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Führt die cURL-Sitzung aus und erhält den Inhalt
+    $response = curl_exec($ch);
+
+    // Schließt die cURL-Sitzung
+    curl_close($ch);
+
+    // Dekodiert die JSON-Antwort und gibt Daten zurück
+    return json_decode($response, true);
+}
+
+echo '<pre>';
+var_dump(fetchEnergyRadioData());
+echo '</pre>';
+
+?>
